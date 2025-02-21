a61a4b2a30280128be90ae94f63016d6c188463c
Gopi Krishna <krishna@Mac.lan>
2025-02-21 16:35:51 -0600

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className="Karate Blogs">
+      <header className="Taang-soo-do">
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello, Welcome to taang soo-do karate!
+          <h2>My journey with Taang soo-do karate</h2>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
 }
-
 export default App;
